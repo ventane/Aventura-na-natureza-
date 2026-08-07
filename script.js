3f65506d33765c409bbadbe1ba42ddbb0aa97821
ventane <amorim.jaylon@escola.pr.gov.br>
2026-08-07 08:50:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+const avanca =
+const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
+avanca.forEach()
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener()
+
+})
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+     })
+})
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute()
+     })
+})
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual - document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+     })
+})
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual - document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        
+        atual.classList.remove('ativo');
+     })
+})
+<!-- código omitido -->
+
+    <script src="script.js"></script>
+</body>
+</html>
